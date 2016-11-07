7a6a59766b69982d2fbb041150a9d245f8337e77
Rafael <rmh287@cornell.edu>
2016-11-06 20:24:43 -0500

    serv_up working and server mli started

---
+++ server.mli
@@ -0,0 +1,20 @@
+type xmpp
+
+(* [process_and_respond in_ch out_ch] is the function which will handle
+ * all requests and write all responses. It is the crux of the server. 
+ * This is the singular function that needs to be implemented--the rest 
+ * are helpers. *)
+val process_and_respond : Unix.in_channel -> Unix.out_channel -> unit
+
+
+(*******************************************************************)
+(******     HELPERS   **********************************************)
+(*******************************************************************)
+
+val convert : Unix.in_channel -> xmpp
+
+val route : xmpp -> unit
+
+val store : data -> data
+
+val fetch : u_id -> data
